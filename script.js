72b531344afcddac1ddb2db9498c219c37f9015d
fnamayi <franklynenm@gmail.com>
2024-11-14 23:25:01 +0300

    fix: formating

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-  // Typing Effect for Profession
-  const professionElement = document.getElementById("profession");
-  const professions = ["Software Engineer", "Developer", "Tech Enthusiast"];
-  let professionIndex = 0;
-  let charIndex = 0;
-  function typeProfession() {
-    if (charIndex < professions[professionIndex].length) {
-      professionElement.innerHTML += professions[professionIndex].charAt(charIndex);
-      charIndex++;
-      setTimeout(typeProfession, 100);
-    } else {
-      setTimeout(() => {
-        professionElement.innerHTML = "";
-        charIndex = 0;
-        professionIndex = (professionIndex + 1) % professions.length;
-        typeProfession();
-      }, 2000);
-    }
+// Typing Effect for Profession
+const professionElement = document.getElementById("profession");
+const professions = ["Software Engineer", "Developer", "Tech Enthusiast"];
+let professionIndex = 0;
+let charIndex = 0;
+function typeProfession() {
+  if (charIndex < professions[professionIndex].length) {
+    professionElement.innerHTML +=
+      professions[professionIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeProfession, 100);
+  } else {
+    setTimeout(() => {
+      professionElement.innerHTML = "";
+      charIndex = 0;
+      professionIndex = (professionIndex + 1) % professions.length;
+      typeProfession();
+    }, 2000);
   }
-  typeProfession();
+}
+typeProfession();
